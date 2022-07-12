5f094fd5e0323f72aeb85d76a52c347f87547374
Mikhail Artamonov <maximalis171091@yandex.ru>
2022-07-12 15:49:48 +0300

    Update

--- main.py
+++ main.py
@@ -3,54 +3,81 @@
 
 # pip install pytube
 # pip install nuitka
+# Windows
+# pip install pyinstaller pywin32 pywin32-ctypes
+# Linux
+# pip install altgraph pyinstaller pyinstaller-hooks-contrib
+#
+# Linux: PyQT5 PyQT5-sip PyQT5-QT PyQT5-QT5 PyQT6 PyQT6-sip PyQT6-QT
+# Windows: PyQT5 PyQT5-sip PyQT6 PyQT6-sip
+#
 # nuitka3 hello.py
+# pyinstaller hello.py
+# --onefile — сборка в один файл, т.е. файлы .dll не пишутся.
+# --windowed -при запуске приложения, будет появляться консоль.
+# --noconsole — при запуске приложения, консоль появляться не будет.
+# --icon=app.ico — добавляем иконку в окно.
+# --paths — возможность вручную прописать путь к необходимым файлам, если pyinstaller
+# не может их найти(например: --paths D:\python35\Lib\site-packages\PyQt5\Qt\bin)
+# pyinstaller --onefile --icon=name.ico --noconsole myscript.py
+# 
 
+import re
 import pathlib
 import pytube
 
-video_size = {1: '240p',
-			2: '360p',
-			3: '480p',
-			4: '720p',
-			5: '1024p'}
+Quality = ''
+isVideoCount = ''
+onVideo = []
+fileLogs = str(pathlib.Path(pathlib.Path.cwd()).resolve().joinpath("logs.txt"))
 
 def on_progress(stream, chunk, bytes_remaining):
+	global isVideoCount
+	global onVideo
+	global fileLogs
 	total_size = stream.filesize
 	bytes_downloaded = total_size - bytes_remaining
 	percentage_of_completion = round(bytes_downloaded / total_size * 100)
 	if percentage_of_completion == 100:
-		print(f"{percentage_of_completion}%\t\t[OK]")
+		print(f"{percentage_of_completion}%\t{isVideoCount}\t[OK]")
 	elif percentage_of_completion < 100 and percentage_of_completion > 0:
-		print(f"{percentage_of_completion}% ...")
+		print(f"{percentage_of_completion}%\t{isVideoCount}")
 	else:
-		print(f"{percentage_of_completion}%\t\t[ERROR]")
+		print(f"Download video {onVideo[0]} ERROR!")
+		with open(fileLogs, 'a') as f:
+			f.write(onVideo[0])
+			f.write(onVideo[1])
+			f.write(onVideo[2])
 
 def filterName(innames: str) -> str:
 	outname = str(innames).replace('|', '.').replace('%', '.').replace(':', '.').replace('"', '.').replace("'", ".").replace('<', '.').replace('>', '.')\
 	.replace('[', '.').replace(']', '.').replace('{', '.').replace('}', '.').replace('#', '.').replace('$', '.').replace('?', '.')\
 	.replace('`', '.').replace('~', '.').replace('@', '.').replace('!', '.').replace('&', '.').replace('^', '.').replace('*', '.')
-	return outname
+	pattern = r'(.)\1+'
+	repl = r'\1'
+	return re.sub(pattern,repl,outname).strip()
 
 def getTitle(urlFile: str) -> str:
+	global Quality
 	yt = pytube.YouTube(urlFile)
-	outname = filterName(yt.title) + '.mp4'
+	Quality = yt.streams.get_highest_resolution().resolution
+	outname = filterName(yt.title) + '_' + Quality + '.mp4'
 	return outname
 
-def downloadYouTube(urlFile: str, yourPath: str = None, FName: str = None, quality: str = '720p'):
+def downloadYouTube(urlFile: str, onPath: str = None, FName: str = None):
 	yt = pytube.YouTube(urlFile, on_progress_callback=on_progress)
-	if quality != 'Undefened':
-		yt = yt.streams.filter(progressive=True, mime_type="video/mp4", res=quality).first()
-	else:
-		yt = yt.streams.filter(progressive=True, mime_type="video/mp4", res='720p').first()
-	yt.download(output_path=yourPath, filename=FName)
+	yt = yt.streams.get_highest_resolution()
+	yt.download(output_path=onPath, filename=FName)
 
-def downloadPlayList(link: str, fullPath: str, size_count: str):
-	global video_size
+def downloadPlayList(link: str, inPath: str):
+	global isVideoCount
+	global onVideo
+	global Quality
 	playlist = pytube.Playlist(link)
 	all_count = str(len(playlist.video_urls))
 	#all_count = len(playlist.video_urls)
 	print('Number of videos in playlist: %s' % all_count)
-	download_path = str(pathlib.Path(fullPath).resolve())
+	download_path = str(pathlib.Path(inPath).resolve())
 	if not pathlib.Path(download_path).exists():
 		pathlib.Path(download_path).mkdir(parents=True, exist_ok=True)
 	video_url = []
@@ -59,25 +86,29 @@ def downloadPlayList(link: str, fullPath: str, size_count: str):
 	index = 1
 	for onURL in video_url:
 		fname = str(index) + '.' + getTitle(onURL)
-		print(f"{fname}\n{index}/{all_count}")
-		downloadYouTube(onURL, download_path, fname, size_count)
+		isVideoCount = f"{index}/{all_count}\t{Quality}"
+		onVideo.clear()
+		onVideo+=[fname]
+		onVideo+=[link]
+		onVideo.append(isVideoCount)
+		print(f"{fname}\n{isVideoCount}")
+		downloadYouTube(onURL, download_path, fname)
 		index+=1
 
-def downloadVideo(link: str, fullPath: str, size_count: str):
-	global video_size
-	download_path = str(pathlib.Path(fullPath).resolve())
+def downloadVideo(link: str, inPath: str):
+	global onVideo
+	download_path = str(pathlib.Path(inPath).resolve())
 	if not pathlib.Path(download_path).exists():
 		pathlib.Path(download_path).mkdir(parents=True, exist_ok=True)
 	fname = getTitle(link)
+	onVideo.clear()
+	onVideo+=[fname]
+	onVideo+=[link]
+	onVideo.append(f"1/1\t{Quality}")
 	print(fname)
-	downloadYouTube(link, download_path, fname, size_count)
+	downloadYouTube(link, download_path, fname)
 
 def main():
-	'''
-	playlist = pytube.Playlist('playlist-url')
-	all_count = str(len(playlist.video_urls))
-	print('Number of videos in playlist: %s' % len(playlist.video_urls))
-	'''
 	'''
 	with open('selfedu.txt', 'w') as the_file:
 		index=1
@@ -88,71 +119,13 @@ def main():
 			the_file.write('\n')
 			index+=1
 	'''
-	'''
-	download_path = str(pathlib.Path(pathlib.Path.cwd()).resolve().joinpath("download-folder-1024p"))
-	if not pathlib.Path(download_path).exists():
-		pathlib.Path(download_path).mkdir(parents=True, exist_ok=True)
-	video_url = [] 
-	for url in playlist.video_urls:
-		video_url.append(url)
-	#video_link = 'video-url'
-	#index = 1
-	#fname = str(index) + '.' + getTitle(video_link)
-	#downloadYouTube(video_link, download_path, fname)
-	#yt = pytube.YouTube(video_link, on_progress_callback=on_progress)
-	#yt = yt.streams.filter(progressive=True, mime_type="video/mp4", res="720p").order_by('resolution').desc().first()
-	#yt = yt.streams.filter(progressive=True, mime_type="video/mp4", res="720p").first() # fps="24fps"
-	#yt.download(download_path)
-	#print(yt.streams)
-	#titles = filterName(yt.title)
-	#print(titles)
-	index = 1
-	for onURL in video_url:
-		fname = str(index) + '.' + getTitle(onURL)
-		print(fname, all_count)
-		downloadYouTube(onURL, download_path, fname, '1024p')
-		index+=1
-	'''
+	# test
+	#url = 'https://www.youtube.com/watch?v=V3h2iq2mylI&list=PLlWXhlUMyoobAlP3mZ0_uuJagsDSg_5YT&index=2'
+	#url = 'https://www.youtube.com/playlist?list=PLlWXhlUMyoobAlP3mZ0_uuJagsDSg_5YT'
+	#folder = '/home/mikl/003/'
+	# print(getTitle(url))
+	#downloadVideo(url, folder)
+	#downloadPlayList(url, folder)
 
 if __name__ == '__main__':
     main()
-
-'''
-In this method you can download the available highest resolution video.
-С помощью этого метода вы можете загрузить доступное видео с самым высоким разрешением.
-
-video = YouTube('mylink')
-highresvid = video.streams.get_highest_resolution()
-highresvid.download('location')
-
-from pytube import YouTube
-yt = YouTube("https://www.youtube.com/watch?t=27&v=RjLH2vE5rpk")
-print(yt.streams.get_highest_resolution().resolution)
-Выход:
-360p
-
-
-
-def downloadYouTube(URLLink: str, yourPath: str, fName: str, quality: str):
-	global video_size
-	yt = pytube.YouTube(URLLink, on_progress_callback=on_progress)
-
-	if switch(quality) == 1: yt.streams.filter(progressive=True, mime_type="video/mp4", res="240p").first()
-	if switch(quality) == 2: yt.streams.filter(progressive=True, mime_type="video/mp4", res="360p").first()
-	if switch(quality) == 3: yt.streams.filter(progressive=True, mime_type="video/mp4", res="480p").first()
-	if switch(quality) == 4: yt.streams.filter(progressive=True, mime_type="video/mp4", res="720p").first()
-	if switch(quality) == 5: yt.streams.filter(progressive=True, mime_type="video/mp4", res="1024p").first()
-	if switch(quality) == None: yt.streams.filter(progressive=True, mime_type="video/mp4", res="720p").first()
-
-	yt.streams.filter(progressive=True, mime_type="video/mp4", res="720p").first()
-	yt.download(output_path=yourPath, filename=fName)
-
-def switch(case):
-	return {
-		"240p": 1,
-		"360p": 2,
-		"480p": 3,
-		"720p": 4,
-		"1024p": 5
-	}.get(case, None)
-'''
