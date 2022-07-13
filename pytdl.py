632c47fb2dc661b9d51670af5dea795e00ce32d4
maximalis171091@yandex.ru <maximalis171091@yandex.ru>
2022-07-13 09:05:03 +0300

    Update

--- pytdl.py
+++ pytdl.py
@@ -5,7 +5,12 @@ import re
 import pathlib
 import pytube
 
-
+class PyTDL(object):
+    """pytube control class"""
+    
+    def __init__(self):
+        """Constructor"""
+        pass
 
 if __name__ == '__main__':
     pass
