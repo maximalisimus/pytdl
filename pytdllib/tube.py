e33ca03c37f829fe58c0a5010cf23b760b0e2ca8
maximalis171091@yandex.ru <maximalis171091@yandex.ru>
2022-07-15 10:08:32 +0300

    Update

--- pytdllib/tube.py
+++ pytdllib/tube.py
@@ -3,21 +3,21 @@ from . import files
 
 '''
 class C:
-    def __init__(self):
-        self._x = None
+	def __init__(self):
+		self._x = None
 
-    @property
-    def x(self): # В декораторах 'setter' и 'deleter' нужно указывать имя метода-свойства
-        """I'm the 'x' property."""
-        return self._x
+	@property
+	def x(self): # В декораторах 'setter' и 'deleter' нужно указывать имя метода-свойства
+		"""I'm the 'x' property."""
+		return self._x
 
-    @x.setter
-    def x(self, value):
-        self._x = value
+	@x.setter
+	def x(self, value):
+		self._x = value
 
-    @x.deleter
-    def x(self):
-        del self._x
+	@x.deleter
+	def x(self):
+		del self._x
 
 '''
 
@@ -87,41 +87,123 @@ class Tube(object):
 		self._isCli = isCli
 
 	@property
-    def url(self) -> str:
+	def url(self) -> str:
 		return self._url
 
 	@property
-    def isPlayList(self) -> bool:
+	def isPlayList(self) -> bool:
 		return self._isPlayList
 
 	@property
-    def loadDir(self) -> str:
+	def loadDir(self) -> str:
 		return self._loadDir
 
 	@property
-    def plFile(self) -> str:
+	def plFile(self) -> str:
 		return self._plFile
 
 	@property
-    def isSaveInfo(self) -> bool:
+	def isSaveInfo(self) -> bool:
 		return self._isSaveInfo
 
 	@property
-    def isSaveURL(self) -> bool:
+	def isSaveURL(self) -> bool:
 		return self._isSaveURL
 
 	@property
-    def isSaveIndex(self) -> bool:
+	def isSaveIndex(self) -> bool:
 		return self._isSaveIndex
 
 	@property
-    def isSaveName(self) -> bool:
+	def isSaveName(self) -> bool:
 		return self._isSaveName
 
 	@property
-    def isSaveQuality(self) -> bool:
+	def isSaveQuality(self) -> bool:
 		return self._isSaveQuality
 
 	@property
-    def isCli(self) -> bool:
+	def isCli(self) -> bool:
 		return self._isCli
+
+	@url.setter
+	def url(self, value):
+		if value != '':
+			self._url = value
+	
+	@url.deleter
+	def url(self):
+		del self._url
+
+	@isPlayList.setter
+	def isPlayList(self, value: bool):
+		self._isPlayList = value
+
+	@isPlayList.deleter
+	def isPlayList(self):
+		del self._isPlayList
+	
+	@loadDirx.setter
+	def loadDirx(self, value: str):
+		self._loadDir = Files.checkPath(value)
+
+	@loadDirx.deleter
+	def loadDir(self):
+		del self._loadDir
+
+	@plFile.setter
+	def plFile(self, value: str):
+		if checkPathText(value):
+			self._plFile = value
+
+	@plFile.deleter
+	def plFile(self):
+		del self._plFile
+
+	@isSaveInfo.setter
+	def isSaveInfo(self, value: bool):
+		self._isSaveInfo = value
+
+	@isSaveInfo.deleter
+	def isSaveInfo(self):
+		del self._isSaveInfo
+
+	@isSaveURL.setter
+	def isSaveURL(self, value: bool):
+		self._isSaveURL = value
+
+	@isSaveURL.deleter
+	def isSaveURL(self):
+		del self._isSaveURL
+	
+	@isSaveIndex.setter
+	def isSaveIndex(self, value: bool):
+		self._isSaveIndex = value
+
+	@isSaveIndex.deleter
+	def isSaveIndex(self):
+		del self._isSaveIndex
+	
+	@isSaveName.setter
+	def isSaveName(self, value: bool):
+		self._isSaveName = value
+
+	@isSaveName.deleter
+	def isSaveName(self):
+		del self._isSaveName
+	
+	@isSaveQuality.setter
+	def isSaveQuality(self, value: bool):
+		self._isSaveQuality = value
+
+	@isSaveQuality.deleter
+	def isSaveQuality(self):
+		del self._isSaveQuality
+	
+	@isCli.setter
+	def isCli(self, value: bool):
+		self._isCli = value
+
+	@isCli.deleter
+	def isCli(self):
+		del self._isCli
