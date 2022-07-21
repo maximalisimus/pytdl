2de6b2c6a16779b728a9fc639fb649f1ea333d31
maximalis171091@yandex.ru <maximalis171091@yandex.ru>
2022-07-21 10:08:52 +0300

    Update

---
+++ tests/test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+import sys
+from pytdllib.basetube import *
+'''
+#from pathlib import Path
+import pathlib
+import sys
+#sys.path.insert(0, str(pathlib.Path('./pytdllib').resolve()))
+#from functions import *
+sys.path.insert(0, str(pathlib.Path('./').resolve()))
+from pytdllib.functions import *
+
+def main():
+	'''
+	for i in sys.path:
+		print(i)
+	'''
+	#print(dir(sys.modules[__name__]))
+	print(dateTimeToStrNow())
+
+if __name__ == '__main__':
+	main()
